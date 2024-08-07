214c8dcf0e4ca71e9378645fd20cc672102a395a
CooKa0 <marcinkuj@gmail.com>
2024-08-07 17:55:58 +0200

    comments

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -1,12 +1,15 @@
 public class SortingAlgorithm {
     public static void insertionSort(long[] array) {
+// start from 2nd element and move it to the end of the array
         for (int i = 1; i < array.length; i++) {
             long key = array[i];
             int previousIndex = i - 1;
+//shift elements of the sorted portion that are greater than the key to the right
             while (previousIndex >= 0 && array[previousIndex] > key) {
                 array[previousIndex + 1] = array[previousIndex];
                 previousIndex = previousIndex - 1;
             }
+//insert the key into correct position
             array[previousIndex + 1] = key;
         }
     }
@@ -16,10 +19,10 @@ public class SortingAlgorithm {
             System.out.println("Please provide a list of numbers to sort.");
             return;
         }
-//array for keeping correct numbers		
+// array for keeping correct numbers		
 		long[] tempArray = new long[args.length];
 		int validCount = 0;
-//adding correct numbers to temporary array		
+// adding correct numbers to temporary array		
 		for (String arg : args) {
 			try{
 				long number = Long.parseLong(arg);
@@ -29,7 +32,7 @@ public class SortingAlgorithm {
 				System.out.println("Ignoring invalid number: " + arg);
 			}
 		}
-		
+// create array of the correct size for valid numbers
         long[] array = new long[validCount];
 		System.arraycopy(tempArray, 0, array, 0, validCount);
 		

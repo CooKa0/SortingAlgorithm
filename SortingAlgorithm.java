ca88472472fadf6ad98ee65fe1cde55d39b95872
CooKa0 <marcinkuj@gmail.com>
2024-08-07 17:37:23 +0200

    #2

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -10,20 +10,35 @@ public class SortingAlgorithm {
             array[previousIndex + 1] = key;
         }
     }
-
+// check if the list is not empty
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Please provide a list of integers to sort.");
             return;
         }
-
-        int[] array = new int[args.length];
-        for (int i = 0; i < args.length; i++) {
-            array[i] = Integer.parseInt(args[i]);
-        }
-
+//array for keeping correct numbers		
+		int[] tempArray = new int[args.length];
+		int validCount = 0;
+//adding correct numbers to temporary array		
+		for (String arg : args) {
+			try{
+				int number = Integer.parseInt(arg);
+				tempArray[validCount] = number;
+				validCount++;
+			} catch (NumberFormatException e) {
+				System.out.println("Ignoring non-integer argument: " + arg);
+			}
+		}
+		
+        int[] array = new int[validCount];
+		System.arraycopy(tempArray, 0, array, 0, validCount);
+		if (array.length == 0) {
+			System.out.println("No valid integers provided to sort.");
+			return;
+		}
+// sorting
         insertionSort(array);
-
+// printing out array
         System.out.println("Sorted array:");
         for (int i : array) {
             System.out.print(i + " ");

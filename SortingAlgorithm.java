650ce0f0f1a85ec56afe794c171b10e62afebb52
CooKa0 <marcinkuj@gmail.com>
2024-08-07 17:48:41 +0200

    changes from int to long

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -1,7 +1,7 @@
 public class SortingAlgorithm {
-    public static void insertionSort(int[] array) {
+    public static void insertionSort(long[] array) {
         for (int i = 1; i < array.length; i++) {
-            int key = array[i];
+            long key = array[i];
             int previousIndex = i - 1;
             while (previousIndex >= 0 && array[previousIndex] > key) {
                 array[previousIndex + 1] = array[previousIndex];
@@ -13,34 +13,35 @@ public class SortingAlgorithm {
 // check if the list is not empty
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Please provide a list of integers to sort.");
+            System.out.println("Please provide a list of numbers to sort.");
             return;
         }
 //array for keeping correct numbers		
-		int[] tempArray = new int[args.length];
+		long[] tempArray = new long[args.length];
 		int validCount = 0;
 //adding correct numbers to temporary array		
 		for (String arg : args) {
 			try{
-				int number = Integer.parseInt(arg);
+				long number = Long.parseLong(arg);
 				tempArray[validCount] = number;
 				validCount++;
 			} catch (NumberFormatException e) {
-				System.out.println("Ignoring non-integer argument: " + arg);
+				System.out.println("Ignoring invalid number: " + arg);
 			}
 		}
 		
-        int[] array = new int[validCount];
+        long[] array = new long[validCount];
 		System.arraycopy(tempArray, 0, array, 0, validCount);
+		
 		if (array.length == 0) {
-			System.out.println("No valid integers provided to sort.");
+			System.out.println("No valid numbers provided to sort.");
 			return;
 		}
 // sorting
         insertionSort(array);
 // printing out array
         System.out.println("Sorted array:");
-        for (int i : array) {
+        for (long i : array) {
             System.out.print(i + " ");
         }
     }
